a273df2ef544e520c3b05711c049ba517bd99c36
Christian Sim <christian@nttaudigital.com>
2025-07-04 09:53:33 +0000

    Workinf with ffe, though very slow

---
+++ steady_state_heat_transport/ssht_heatnet.py
@@ -0,0 +1,86 @@
+#heatnet
+
+import torch
+import torch.nn as nn
+
+from ssht_ffe import FourierFeatureEmbedding
+
+
+def np_to_torch(x):
+    #n_samples = len(x)
+    return torch.from_numpy(x).to(torch.float32).reshape(-1, 1)  # changed from ...reshape(n_samples, -1)
+
+class HeatNet(nn.Module):
+    def __init__(self, input_dim=2, output_dim=1, n_units=40, epochs=1000, loss=nn.MSELoss, lr=0.001, 
+                 pde_loss=None, bc_loss=None, pde_weight=1.0, bc_weight=1.0,
+                 use_ffe=True, num_frequencies=16, fourier_scale=1.0):
+        super().__init__()
+        self.epochs = epochs
+        self.loss = loss 
+        self.lr = lr
+        self.pde_loss = pde_loss
+        self.bc_loss = bc_loss
+        self.pde_weight = pde_weight
+        self.bc_weight = bc_weight
+        self.use_ffe = use_ffe
+
+        if use_ffe:
+            self.fourier = FourierFeatureEmbedding(input_dim, num_frequencies, fourier_scale)
+            input_dim = num_frequencies * 2 #note input_dim is the input into the training loop (ie after ffe)
+        else:
+            self.fourier = None
+
+        self.layers = nn.Sequential(
+            nn.Linear(input_dim, n_units), ################
+            nn.Tanh(),
+            nn.Linear(n_units, n_units),
+            nn.Tanh(),
+            nn.Linear(n_units, n_units),
+            nn.Tanh(),
+            nn.Linear(n_units, n_units),
+            nn.Tanh(),
+            nn.Linear(n_units, output_dim)
+        )
+
+
+    def forward(self, xy): #forward takes a 1d input so take concatenation of x,y
+        if self.fourier:
+            xy = self.fourier(xy)
+        return self.layers(xy)
+    
+    def fit(self, x_domain_np, y_domain_np, x_bc, y_bc, t_bc):
+        
+        x_domain = torch.from_numpy(x_domain_np).to(torch.float32).reshape(-1, 1) 
+        y_domain = torch.from_numpy(y_domain_np).to(torch.float32).reshape(-1, 1) 
+
+        x_bc = np_to_torch(x_bc)
+        y_bc = np_to_torch(y_bc)
+        t_bc = np_to_torch(t_bc) # t_bc is the temperature target value at each boundary point
+
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        self.train()
+        losses = []
+
+        for ep in range(self.epochs):
+            # Create new tensors with requires_grad=True for autograd
+            x_domain_torch = x_domain.clone().detach().requires_grad_(True)
+            y_domain_torch = y_domain.clone().detach().requires_grad_(True)# removed from both .view(-1, 1)
+            optimizer.zero_grad()
+            loss = 0.0
+
+            if self.pde_loss:
+                lpde = self.pde_weight * self.pde_loss(self, x_domain_torch, y_domain_torch)
+                loss += lpde   ###last thing i changed
+
+            if self.bc_loss:
+                lbc = self.bc_weight * self.bc_loss(self, x_bc, y_bc, t_bc)
+                loss += lbc
+
+            loss.backward()
+            optimizer.step()
+            losses.append(loss.item())
+        
+            if ep % int(self.epochs/10)==0:
+                print(f"Epoch {ep}: Loss (tot) {loss.item():.3f}, Loss (pde) {lpde}, Loss (bc) {lbc} ")
+        
+        return losses
---
+++ steady_state_heat_transport/ssht_loss_fns.py
@@ -0,0 +1,28 @@
+#loss_fns
+
+import torch
+
+def pde_loss_2d(model, x, y):
+
+    xy = torch.cat([x, y], dim=1)
+    T = model(xy)
+
+    #print(f"T: {T}")
+
+    T_x = torch.autograd.grad(T.sum(), x, create_graph=True)[0]
+    T_y = torch.autograd.grad(T.sum(), y, create_graph=True)[0]
+
+    #print(f"T_x: {T_x}")
+
+    T_xx = torch.autograd.grad(T_x.sum(), x, create_graph=True)[0]
+    T_yy = torch.autograd.grad(T_y.sum(), y, create_graph=True)[0]
+
+    #print(f"T_xx: {T_xx}")
+
+    return ((T_xx + T_yy) ** 2).mean()
+
+def bc_loss_2d(model, x_bc, y_bc, t_bc):
+    xy = torch.cat([x_bc, y_bc], dim=1)
+    T = model(xy)
+
+    return ((T - t_bc) ** 2).mean()
